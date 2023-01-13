102cde11c735a3552c0d0867b8136a7cccad94ea
afikri <fikri.abdul@gmail.com>
2023-01-13 16:11:45 +0700

    Removed card image

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
-import { Favorite, FavoriteBorder, MoreVert, Share } from "@mui/icons-material";
 import {
   Box,
   Card,
-  CardActions,
-  CardContent,
-  CardHeader,
   CardMedia,
-  Checkbox,
-  IconButton,
   Stack,
-  Typography,
 } from "@mui/material";
 
 const Home = () => {
@@ -22,41 +15,13 @@ const Home = () => {
       <Stack direction="row" spacing={2} justifyContent="space-between">
         <Sidebar />
         <Box flex={4} p={{ xs: 0, md: 2 }}>
-          <Card sx={{ margin: 5 }}>
-            <CardHeader
-             
-              action={
-                <IconButton aria-label="settings">
-                  <MoreVert />
-                </IconButton>
-              }
-              title="John Doe"
-              subheader="September 14, 2022"
-            />
+          <Card>
             <CardMedia
               component="img"
               height="20%"
               image="https://images.pexels.com/photos/4534200/pexels-photo-4534200.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
               alt="Paella dish"
             />
-            <CardContent>
-              <Typography variant="body2" color="text.secondary">
-                This impressive paella is a perfect party dish and a fun meal to
-                cook together with your guests. Add 1 cup of frozen peas along
-                with the mussels, if you like.
-              </Typography>
-            </CardContent>
-            <CardActions disableSpacing>
-              <IconButton aria-label="add to favorites">
-                <Checkbox
-                  icon={<FavoriteBorder />}
-                  checkedIcon={<Favorite sx={{ color: "red" }} />}
-                />
-              </IconButton>
-              <IconButton aria-label="share">
-                <Share />
-              </IconButton>
-            </CardActions>
           </Card>
         </Box>
       </Stack>
